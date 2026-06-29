df45fae1d38891a48fe0e543f1635ec4e9fd3e48
wam <dorothy_007@naver.com>
2026-06-29 15:10:30 +0900

    feat: 환경 변수 유효성 검사 추가 및 TypeORM 연동

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,36 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as Joi from 'joi';
 import { GroupModule } from './group/group.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // ConfigModule이 전역으로 설정되어야 하는지 여부
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+      ignoreEnvFile: process.env.NODE_ENV === 'prod', // 서버에 deploy 할 때 환경변수 파일을 사용하지 않기
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('dev', 'prod').required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_DATABASE: Joi.string().required(),
+      }),
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT!),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      synchronize: true,
+      logging: true,
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: true, // 파일을 직접 갖을 필요 없어서 변경
